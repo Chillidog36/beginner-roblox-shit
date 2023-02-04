faf0574872da201ddf38dfe33ba595de70035e97
Chillidog36 <65378645+Chillidog36@users.noreply.github.com>
2023-02-03 23:16:47 -0600

    very shit and un-updated

---
+++ first.lua
@@ -0,0 +1,13 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Chilli's roblox shit", "Ocean")
+
+-- fps
+local fps = Window:NewTab("FPS GAMES")
+local fps = fps:NewSection("Aimbots")
+
+fps:NewButton("FPS GUI", "universl aimbot with gui", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V2/main/Resources/Scripts/Aimbot%20V2%20GUI.lua"))()
+end)
+fps:NewButton("ESP GUI", "universl aimbot with gui", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua',true))()
+end)
